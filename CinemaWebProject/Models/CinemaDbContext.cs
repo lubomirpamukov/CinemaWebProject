afdde660416b980cae641c3530c3124b37dc116b
Lubomir <lubomir.pamukov@gmail.com>
2024-10-17 18:02:38 +0300

    Added Identity User and scaffolded register login and logout functionality

--- CinemaWebProject/Models/CinemaDbContext.cs
+++ CinemaWebProject/Models/CinemaDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaWebProject.Models;
 
-public class CinemaDbContext : DbContext
+public class CinemaDbContext : IdentityDbContext<IdentityUser>
 {
     public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
     {
