e0c4e1ae26ddac53872fa45f3ff5da1fe32cda4c
Lubomir <lubomir.pamukov@gmail.com>
2024-10-17 18:59:03 +0300

    Connected IdentityUser to UserMovies mapping table

--- CinemaWebProject/Models/CinemaDbContext.cs
+++ CinemaWebProject/Models/CinemaDbContext.cs
@@ -11,9 +11,11 @@ public class CinemaDbContext : IdentityDbContext<IdentityUser>
 
     }
 
-    public DbSet<Movie> Movies { get; set; }
-    public DbSet<Cinema> Cinemas { get; set; }
-    public DbSet<CinemaMovie> CinemasMovies { get; set; }
+    public virtual DbSet<Movie> Movies { get; set; }
+    public virtual DbSet<Cinema> Cinemas { get; set; }
+    public virtual DbSet<CinemaMovie> CinemasMovies { get; set; }
+    public virtual DbSet<UserMovie> UsersMovies { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -28,12 +30,30 @@ public class CinemaDbContext : IdentityDbContext<IdentityUser>
             .HasOne(x => x.Cinema)
             .WithMany(x => x.CinemaMovies)
             .HasForeignKey(x => x.CinemaId)
-            .OnDelete(DeleteBehavior.NoAction);
+            .OnDelete(DeleteBehavior.Cascade);
 
         modelBuilder.Entity<CinemaMovie>()
             .HasOne(x => x.Movie)
             .WithMany(x => x.CinemaMovies)
             .HasForeignKey(x => x.MovieId)
-            .OnDelete(DeleteBehavior.NoAction);
+            .OnDelete(DeleteBehavior.Cascade);
+
+        //Define the composite key for UserMovie
+        modelBuilder.Entity<UserMovie>()
+            .HasKey(um => new { um.MovieId, um.UserId });
+
+        //Configure relation between UserMovie and IdentityUser
+        modelBuilder.Entity<UserMovie>()
+            .HasOne(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        //Configure the realtion between UserMovie and Movie
+        modelBuilder.Entity<UserMovie>()
+            .HasOne(x => x.Movie)
+            .WithMany()
+            .HasForeignKey(x => x.MovieId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
---
+++ CinemaWebProject/Models/UserMovie.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CinemaWebProject.Models;
+
+public class UserMovie
+{
+    public string UserId { get; set; } = null!;
+
+    public virtual IdentityUser User { get; set; } = null!;
+
+    public int MovieId { get; set; }
+
+    public Movie Movie { get; set; } = null!;
+}
