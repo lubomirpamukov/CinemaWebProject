1b525e153bdc6ba1d0720925993963445bea8c9c
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-12 17:22:43 +0200

    Added GetAllMoviesAsync service

---
+++ CinemaWeb.Services/Interfaces/IMovieService.cs
@@ -0,0 +1,17 @@
+﻿using CinemaWebProject.Models;
+using CinemaWebProject.ViewModels.Movie;
+
+namespace CinemaWeb.Services.Interfaces;
+
+public  interface IMovieService
+{
+    public Task <IEnumerable<MovieIndexViewModel>> GetAllMoviesAsync();
+
+    public Task<Movie> CreateAsync(MovieCreateViewModel movie);
+
+    public Task<MovieDetailsViewModel> GetDetailsAsync(int id);
+
+    public Task <bool> AddToProgramAsync(int id);
+
+
+}
---
+++ CinemaWeb.Services/Services/MovieService.cs
@@ -0,0 +1,48 @@
+﻿using CinemaWeb.Data.Models;
+using CinemaWeb.Services.Interfaces;
+using CinemaWebProject.Models;
+using CinemaWebProject.ViewModels.Movie;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaWeb.Services.Services;
+
+public class MovieService(CinemaDbContext dbContext) : IMovieService
+{
+    private readonly CinemaDbContext _context = dbContext;
+    public Task<bool> AddToProgramAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Movie> CreateAsync(MovieCreateViewModel movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<MovieIndexViewModel>> GetAllMoviesAsync()
+    {
+        var viewModel = new List<MovieIndexViewModel>();
+        var movies = await _context.Movies.ToListAsync();
+
+        foreach (var movie in movies)
+        {
+            var movieView = new MovieIndexViewModel
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Duration = movie.Duration,
+                Genre = movie.Genre,
+                ReleaseDate = movie.ReleaseDate,
+            };
+
+            viewModel.Add(movieView);
+        }
+
+        return viewModel;
+    }
+
+    public Task<MovieDetailsViewModel> GetDetailsAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
