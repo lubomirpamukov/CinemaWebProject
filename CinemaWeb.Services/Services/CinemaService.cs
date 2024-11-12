68fb01234bd1cbaf43ec3821fe7153f6feb5a10a
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-12 21:48:39 +0200

    Implement GetAllAsync method

---
+++ CinemaWeb.Services/Services/CinemaService.cs
@@ -0,0 +1,24 @@
+﻿
+
+using CinemaWeb.Data.Models;
+using CinemaWeb.Services.Interfaces;
+using CinemaWeb.ViewModels.Cinema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaWeb.Services.Services;
+
+public class CinemaService(CinemaDbContext context) : ICinemaService
+{
+    private readonly CinemaDbContext _context = context;
+    public async Task<IEnumerable<CinemaIndexViewModel>> GetAllAsync()
+    {
+        var viewModel = await _context.Cinemas.Select(c => new CinemaIndexViewModel
+        {
+            Id = c.Id,
+            Name = c.Name,
+            Location = c.Location
+        }).ToListAsync();
+
+        return viewModel;
+    }
+}
