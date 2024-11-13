3f6016235e504980b9e92c64007e8dd4fed2239b
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-13 11:45:06 +0200

    Added navigation colelctin for tickets

--- CinemaWeb.Data.Models/Models/ApplicationUser.cs
+++ CinemaWeb.Data.Models/Models/ApplicationUser.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CinemaWeb.Data.Models.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace CinemaWeb.Models;
 
 public class ApplicationUser : IdentityUser
 {
     public  string? BillingInformation { get; set; }
+    public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
 }
