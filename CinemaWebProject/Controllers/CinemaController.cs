7e7c826240d0bd604b56fc3248dff3d156ec1cc8
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-13 10:34:25 +0200

    Refactor Cinema Controller to use ViewMovieProgram service

--- CinemaWebProject/Controllers/CinemaController.cs
+++ CinemaWebProject/Controllers/CinemaController.cs
@@ -44,25 +44,11 @@ public class CinemaController(CinemaDbContext context, ICinemaService cinemaServ
 
     public async Task<IActionResult> ViewMovieProgram(int id) 
     {
-        CinemaViewMovieProgramViewModel? cinemaViewMoviesViewModel = await _context.Cinemas
-            .Where(cinema => cinema.Id == id) // filter only the needed movie
-            .Include(cinema => cinema.CinemaMovies) // include the mapping table in the cinema model
-            .ThenInclude(cinemaMovie => cinemaMovie.Movie) // here were in the mapping table and include each movie
-            .Select(cinema => new CinemaViewMovieProgramViewModel // making a projection and mapping data from the
-            {                                                        //database model to the viewModel
-                Id = cinema.Id,
-                Name = cinema.Name,
-                Location = cinema.Location,
-                Movies = cinema.CinemaMovies.Select(cinemaMovie => new MovieProgramViewModel
-                { // filling up the movie program for the cinema
-                    Title = cinemaMovie.Movie.Title, // from the cinema movie mapping table in cinema i select the movie and take the title
-                    Duration = cinemaMovie.Movie.Duration // // from the cinema movie mapping table in cinema i select the movie and take the title
-                }).ToList()
-            }).FirstOrDefaultAsync();
+        var cinemaViewMoviesViewModel = await _cinemaService.ViewMovieProgramAsync(id);
 
-        if (cinemaViewMoviesViewModel == null)
+        if (cinemaViewMoviesViewModel == null) 
         {
-            return RedirectToAction("Index");
+            return NotFound();
         }
 
         return View(cinemaViewMoviesViewModel);
