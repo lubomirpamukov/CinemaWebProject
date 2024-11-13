221329a14a9239531b16fdf8a0362453f63f410e
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-13 17:28:15 +0200

    Implemented repository pattern

---
+++ CinemaWeb.Infrastructure/Repository.cs
@@ -0,0 +1,87 @@
+﻿using CinemaWeb.Infrastructure.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinemaWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaWeb.Infrastructure;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly CinemaDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(CinemaDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public Task<IQueryable<T>> GetAllAttachedAsync()
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<T> FindByIdAsync(object id)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> AddAsync(T entity)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> AddRangeAsync(IEnumerable<T> entities)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> UpdateAsync(T entity)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> SoftDeleteAsync(T entity)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> SoftDeleteRangeAsync(IEnumerable<T> entities)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> DeleteAsync(T entity)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> DeleteRangeAsync(IEnumerable<T> entities)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<(IEnumerable<T> Items, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<bool> ExistsAsync(Func<T, bool> predicate)
+    {
+        throw new ArgumentException();
+    }
+
+    public Task<int> CountAsync()
+    {
+        throw new ArgumentException();
+    }
+}
---
+++ CinemaWeb.Infrastructure/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaWeb.Infrastructure.Repository.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    public Task<IEnumerable<T>> GetAllAsync();
+
+    public Task<IQueryable<T>> GetAllAttachedAsync();
+
+    public Task<T> FindByIdAsync(object id);
+
+    public Task<bool> AddAsync(T entity);
+
+    public Task<bool> AddRangeAsync(IEnumerable<T> entities);
+
+    public Task<bool> UpdateAsync(T entity);
+
+    public Task<bool> SoftDeleteAsync(T entity);
+
+    public Task<bool> SoftDeleteRangeAsync(IEnumerable<T> entities);
+
+    public Task<bool> DeleteAsync(T entity);
+
+    public Task<bool> DeleteRangeAsync(IEnumerable<T> entities);
+
+    public Task<(IEnumerable<T> Items, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize);
+
+    public Task<bool> ExistsAsync(Func<T, bool> predicate);
+
+    public Task<int> CountAsync();
+
+
+}
