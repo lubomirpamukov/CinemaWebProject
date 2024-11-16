c5eced4fac4cfdb056027742f2536b0f05b04d33
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-16 23:40:37 +0200

    implemented IWatchlistService

---
+++ CinemaWeb.Services/Interfaces/IWatchlistService.cs
@@ -0,0 +1,15 @@
+﻿using CinemaWeb.ViewModels.ViewModels.Watchlist;
+
+namespace CinemaWeb.Services.Interfaces;
+
+public interface IWatchlistService
+{
+    // Index
+    public Task<IEnumerable<WatchListViewModel>> GetWatchlistAsync(string userId);
+   
+    //AddToWatchList
+    public Task<bool> AddToWatchlistAsync(string userId, int movieId);
+
+    //RemoveFromWatchlist
+    public Task<bool> RemoveFromWatchlistAsync(string userId,int movieId);
+}
---
+++ CinemaWeb.Services/Services/WatchlistService.cs
@@ -0,0 +1,63 @@
+﻿using CinemaWeb.Infrastructure.Repository.Interfaces;
+using CinemaWeb.Models;
+using CinemaWeb.Services.Interfaces;
+using CinemaWeb.ViewModels.ViewModels.Watchlist;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaWeb.Services.Services;
+
+public class WatchlistService(IRepository<UserMovie> usersMovies) : IWatchlistService
+{
+    private readonly IRepository<UserMovie> _usersMovies = usersMovies;
+    public async Task<bool> AddToWatchlistAsync(string userId, int movieId)
+    {
+        var movieToAdd = await _usersMovies
+            .GetAllAttachedAsync()
+            .FirstOrDefaultAsync(um => um.UserId == userId && um.MovieId == movieId);
+
+        if (movieToAdd == null)
+        {
+            UserMovie userMovieToAdd = new UserMovie
+            {
+                MovieId = movieId,
+                UserId = userId,
+            };
+
+            return await _usersMovies.AddAsync(userMovieToAdd);
+        }
+
+        return false;
+    }
+
+    public async Task<IEnumerable<WatchListViewModel>> GetWatchlistAsync(string userId)
+    {
+        var watchlistMovies = await _usersMovies
+                .GetAllAttachedAsync()
+                .Where(um => um.UserId == userId)
+                .Include(um => um.Movie)
+                .Select(x => new WatchListViewModel
+                {
+                    Title = x.Movie.Title,
+                    Genre = x.Movie.Genre!,
+                    ImageUrl = x.Movie.ImageUrl,
+                    MovieId = x.MovieId,
+                    ReleaseDate = x.Movie.ReleaseDate.ToString("yyyy-MM-dd")
+                }).ToArrayAsync();
+
+        return watchlistMovies;
+    }
+
+    public async Task<bool> RemoveFromWatchlistAsync(string userId, int movieId)
+    {
+        var movieToRemove = _usersMovies
+                .GetAllAttachedAsync()
+                .FirstOrDefault(um => um.MovieId == movieId && um.UserId == userId);
+
+        if (movieToRemove == null)
+        {
+            throw new ArgumentNullException(nameof(movieToRemove), "No movie to remove or user lack authority to remove movie");
+        }
+
+        return await _usersMovies.DeleteAsync(movieToRemove);
+    }
+}
