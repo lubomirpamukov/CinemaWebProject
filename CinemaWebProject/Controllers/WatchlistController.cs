bbd88a33ad1bbc8e8c455d8d04c821a6545040e7
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-16 21:20:33 +0200

    Implemented Index , AddtoWatcchlist and RemoveFromWatchlist methods to controller

--- CinemaWebProject/Controllers/WatchlistController.cs
+++ CinemaWebProject/Controllers/WatchlistController.cs
@@ -36,8 +36,48 @@ namespace CinemaWebProject.Controllers
 		[HttpPost]
 		public async Task<IActionResult> AddToWatchList(int movieId) 
 		{
-			return NotFound();
+			var userId = _userManager.GetUserId(User);
+
+			if(userId == null) 
+			{
+				throw new ArgumentNullException(nameof(userId));
+			}
+
+			var movieToAdd = await _context.UsersMovies
+				.FirstOrDefaultAsync(um => um.UserId == userId && um.MovieId == movieId);
+
+			if (movieToAdd == null)
+			{
+                UserMovie userMovieToAdd = new UserMovie
+                {
+                    MovieId = movieId,
+                    UserId = userId,
+                };
+
+                await _context.UsersMovies.AddAsync(userMovieToAdd);
+                await _context.SaveChangesAsync();
+            }
+
+			return RedirectToAction("Index", "Movie");
 		}
 
+		[HttpPost]
+		public async Task<IActionResult> RemoveFromWatchlist(int movieId) 
+		{
+			var userId = _userManager.GetUserId(User);
+
+			var movieToRemove = _context.UsersMovies
+				.FirstOrDefault(um => um.MovieId == movieId && um.UserId == userId);
+
+			if (movieToRemove == null)
+			{ 
+				throw new ArgumentNullException(nameof(movieToRemove), "No movie to remove or user lack authority to remove movie");
+			}
+
+			_context.UsersMovies.Remove(movieToRemove);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("Index");
+		}
     }
 }
