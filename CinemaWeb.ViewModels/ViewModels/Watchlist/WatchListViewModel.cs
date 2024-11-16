6de7a84d2b8abac64010e83bca2dca35d614430d
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-16 15:28:08 +0200

    Added WatchListVewModel

---
+++ CinemaWeb.ViewModels/ViewModels/Watchlist/WatchListViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CinemaWeb.ViewModels.ViewModels.Watchlist;
+
+public class WatchListViewModel
+{
+    public int MovieId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Genre { get; set; } = null!;
+
+    public string ReleaseDate { get; set; } = null!;
+
+    public string ImageUrl { get; set; } = null!;
+}
