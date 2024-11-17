4a6ff370989f3977f4538f9c04d56fb8903ac885
lubomirpamukov <lubomir.pamukov@gmail.com>
2024-11-17 22:13:43 +0200

    Added softdelete to Cinema

---
+++ CinemaWeb.Data/Migrations/20241117201123_CinemaSoftDeleteImplemented.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaWeb.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CinemaSoftDeleteImplemented : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Cinemas",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Cinemas");
+        }
+    }
+}
